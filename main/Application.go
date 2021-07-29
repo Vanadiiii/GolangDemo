9e75e742c0ee42c742b993cf198668304cc5d9e5
Ivan Matveev <vanadiii42@gmail.com>
2021-07-30 00:47:58 +0400

    [cycle] for-cycle and if-condition

--- main/Application.go
+++ main/Application.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, world!")
+
+	for i := 0; i < 10; i++ {
+		fmt.Print(i)
+		if i == 5 {
+			fmt.Println(" - this is my favourite number - ")
+		} else {
+			fmt.Println(" - number")
+		}
+	}
 }
